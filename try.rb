31a6678598ad9b0bb5f62a389657b907d6bcc595
Jiaming Xu <zjuxjm@gmail.com>
2017-01-10 06:24:40 -0500

    remove

--- try.rb
+++
@@ -1,9 +0,0 @@
-
-require 'JSON'
-
-class Try
-@t = JSON.parse([{"independent_study":true,"name":"Senior Essay","subjects":[{"segment":"1","id":"1161-1900"}],"code":"ENVS 97A","credits":null,"description":"Usually offered every year.\nStaff","requirements":[]}])
-
-printf @t
-
-end
